4dfd0bd68a195581b0e04d10ccdaddb51da1c24a
Tuxzx <tuxzx@outlook.com>
2017-12-30 16:13:55 +0800

    修复选择城市失败:level参数相同

--- app/src/main/java/com/example/tuxzx/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/tuxzx/coolweather/ChooseAreaFragment.java
@@ -36,8 +36,8 @@ import okhttp3.Response;
 
 public class ChooseAreaFragment extends Fragment {
     public static final int LEVEL_PROVINCE = 0;
-    public static final int LEVEL_CITY = 0;
-    public static final int LEVEL_COUNTY = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
 
     private ProgressDialog progressDialog;
     private TextView titleText;
