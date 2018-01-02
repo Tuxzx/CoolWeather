396ce1deb9250c009d0f96175aea47c27f91ce81
Tuxzx <tuxzx@outlook.com>
2018-01-02 20:18:54 +0800

    更换key

--- app/src/main/java/com/example/tuxzx/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/tuxzx/coolweather/WeatherActivity.java
@@ -128,7 +128,7 @@ public class WeatherActivity extends AppCompatActivity {
 
     //根据天气id请求城市天气信息
     public void requestWeather(final String weatherId) {
-        String weatherUrl = "http://guolin.tech/api/weather?cityid="+weatherId+"&key=bc0418b57b2d4918819d3974ac1285d9";
+        String weatherUrl = "http://guolin.tech/api/weather?cityid="+weatherId+"&key=7bc2d739e1d04d20823c38e4dc75bae3";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
